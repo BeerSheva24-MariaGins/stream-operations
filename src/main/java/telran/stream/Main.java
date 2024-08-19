531edef08af8cb21d8d91e91b46d9410c5a2ca55
MariaGins <m.gins@mail.ru>
2024-08-19 15:51:31 +0300

    added changes

--- src/main/java/telran/stream/Main.java
+++ src/main/java/telran/stream/Main.java
@@ -1,12 +1,26 @@
 package telran.stream;
 
-import java.util.Arrays;
-import java.util.Random;
+// import java.util.Arrays;
+// import java.util.Random;
 
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println();
+        
+//         new Random().ints(6, 1, 50).distinct().limit(6).forEach(n -> System.out.print(n + " "));
+//     }
+// }
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        
-        new Random().ints(6, 1, 50).distinct().limit(6).forEach(n -> System.out.print(n + " "));
+        int message = 560;
+        int maska = 67;
+        int codedMessage = message ^ maska;
+        int receivedMesssage = codedMessage ^ maska;
+        System.out.println("message = " + message);
+        System.out.println("message = " + Integer.toBinaryString(message));
+        System.out.println("codedMessage = " + codedMessage);
+        System.out.println("codedMessage = " + Integer.toBinaryString(codedMessage));
+        System.out.println("receivedMesssage = " + receivedMesssage);
+        System.out.println("receivedMesssage = " + Integer.toBinaryString(receivedMesssage));
     }
 }
--- src/test/java/telran/stream/StreamTasksTest.java
+++ src/test/java/telran/stream/StreamTasksTest.java
@@ -10,16 +10,13 @@ import org.junit.jupiter.api.Test;
 public class StreamTasksTest {
     @Test
     void shuffleTest() {
-        int[] original = {1, 2, 8, 4, 5, 9};
+        int[] original = {1, 11, 8, 4, 5, 9};
         int[] shuffled = StreamTasks.shuffle(original.clone());
         
-        // Вывод массива shuffled на консоль
-        System.out.println(Arrays.toString(shuffled));
+        System.out.println(Arrays.toString(shuffled));        
         
-        // Проверка, что длина массивов совпадает
         assertEquals(original.length, shuffled.length);
-        
-        // Проверка, что массивы содержат одни и те же элементы
+       
         Arrays.sort(original);
         Arrays.sort(shuffled);
         assertArrayEquals(original, shuffled);
